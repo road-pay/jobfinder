6e6915f81d74220982de4dae14ff384d9dd0d1c8
road-pay <230705189@student.ar-raniry.ac.id>
2025-12-27 02:47:22 +0700

    revisi code

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Employer; 
 
 class Job extends Model
 {
-    //
+    use HasFactory;
+
+    // Jurus Anti-Ribet:
+    // Ini artinya: "Jangan ada kolom yang dijaga, izinkan semua data masuk"
+    protected $guarded = [];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
